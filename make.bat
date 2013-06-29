2a87eae52ebf741de673f88037ef4bb3e51ff46f
Alin Mindroc <mindroc.alin@gmail.com>
2013-06-29 15:32:31 +0300

    no message

--- make.bat
+++ make.bat
@@ -1,7 +1,10 @@
 @echo on
 
+::insert bash path here
 @set BASHPATH="C:\cygwin\bin\bash"
+::insert project path here
 @set PROJECTDIR="/cygdrive/C/Users/Alin/Desktop/OpenCV-2.4.5-android-sdk/OpenCV-2.4.5-android-sdk/samples/face-detection"
+::insert NDK path here
 @set NDKDIR="/cygdrive/d/android-ndk-r8e/ndk-build"
 
 %BASHPATH% --login -c "cd %PROJECTDIR% && %NDKDIR%
