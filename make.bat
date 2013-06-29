74fc57c9a83670562777e7241f0f35ac2e849b25
Alin Mindroc <mindroc.alin@gmail.com>
2013-06-29 15:01:33 +0300

    first commit

---
+++ make.bat
@@ -0,0 +1,9 @@
+@echo on
+
+@set BASHPATH="C:\cygwin\bin\bash"
+@set PROJECTDIR="/cygdrive/C/Users/Alin/Desktop/OpenCV-2.4.5-android-sdk/OpenCV-2.4.5-android-sdk/samples/face-detection"
+@set NDKDIR="/cygdrive/d/android-ndk-r8e/ndk-build"
+
+%BASHPATH% --login -c "cd %PROJECTDIR% && %NDKDIR%
+
+@pause:
---
+++ obj/local/armeabi/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/jni.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/assert.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/string.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/strings.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/memory.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/math.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/limits.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ /cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/jni.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/string.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/math.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+/cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/cygdrive/d/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/cygdrive/d/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/log.h:
